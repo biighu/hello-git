64165d9f99e50b61a81312f059d12b6eaa483236
胡威 <huwei@chaoxing.com>
2019-12-10 22:50:54 +0800

    night

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hi~");
+        say("yumeng");
     }
+
+    static void say(String hello) {
+        System.out.println("hello " + hello);
+    }
+
 }
